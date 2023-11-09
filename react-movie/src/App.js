373bdfafbe2d1aae29b996ba7429b6ee3860da98
Henrimusta <mustakangas.henri@gmail.com>
2023-11-09 10:28:26 +0200

    Etusivu
    Etusivun pohja, vaatii vielä muokkausta

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
-import { MovieList } from './functions'; // Import from functions.js
+import { MovieList } from './functions';
+import { useEffect } from 'react';
+import './App.css'
+import SearchIcon from './search.svg'
+import MovieCard from './MovieCard';
+
+// Import from functions.js
 
 // ReadMe - React1 video käy läpi tämän ja muiden tiedostojen sisällön
 
-
+/*
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -39,8 +45,66 @@ return (
     )}
   </div>
 );
+}*/
+
+const API_URL = 'http://www.omdbapi.com?apikey=d4f64de4'
+
+const movie1 = {
+  "Title": "Spiderman",
+  "Year": "2010",
+  "imdbID": "tt1785572",
+  "Type": "movie",
+  "Poster": "N/A"
 }
 
+const App = () => {
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  }
+
+
+  useEffect(() => {
+
+    searchMovies('Spiderman')
+
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>NWADB</h1>
+      <div className="search">
+        <input placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0
+        ? (<div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )};
+    </div>
+  );
+}
 
 
 export default App;
