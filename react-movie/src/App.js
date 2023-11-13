24edd9e149bf9401b973834e4fbd41874dba565f
Toni Isopoussu <toni.isopoussu@outlook.com>
2023-11-13 11:02:03 +0200

    update
    update

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -2,22 +2,15 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
-import { Login } from './components/Auth'; // Adjust the path as necessary
+import { Login } from './components/Auth';
 import { jwtToken, userData } from './components/Signals';
 
 const API_URL = 'http://www.omdbapi.com?apikey=d4f64de4';
 
-// const movie1 = {
-//   "Title": "Spiderman",
-//   "Year": "2010",
-//   "imdbID": "tt1785572",
-//   "Type": "movie",
-//   "Poster": "N/A"
-// }
-
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [activeTab, setActiveTab] = useState('home'); // Aktiivisen välilehden tila
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -30,44 +23,59 @@ const App = () => {
   }, []);
 
   const handleLogout = () => {
-    jwtToken.value = ''; // Clear the token which effectively logs the user out
-    userData.value = null; // Clear user data
+    jwtToken.value = ''; // Kirjaudu ulos
+    userData.value = null;
+    setActiveTab('home'); // Palaa etusivulle uloskirjautumisen jälkeen
   };
 
   return (
     <div className="app">
       <h1>NWADB</h1>
-      {/* Display login or logout button based on jwtToken */}
-      {jwtToken.value.length === 0 ? (
-        <Login />
-      ) : (
-        <button onClick={handleLogout}>Logout</button>
-      )}
 
-      <div className="search">
-        <input
-          placeholder="Search for movies"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={() => searchMovies(searchTerm)}
-        />
+      {/* Nappulat välilehtien vaihtamiseen */}
+      <div>
+        <button onClick={() => setActiveTab('home')}>Home</button>
+        {jwtToken.value.length === 0 && (
+          <button onClick={() => setActiveTab('auth')}>Log In</button>
+        )}
+        {jwtToken.value.length > 0 && (
+          <button onClick={handleLogout}>Log Out</button>
+        )}
       </div>
 
-      {movies.length > 0 ? (
-        <div className="container">
-          {movies.map((movie, index) => (
-            <MovieCard key={index} movie={movie} /> // Ensure MovieCard is properly defined or imported
-          ))}
-        </div>
-      ) : (
-        <div className="empty">
-          <h2>No movies found</h2>
+      {/* Ehdollinen sisältö aktiivisen välilehden perusteella */}
+      {activeTab === 'home' && (
+        <div>
+          {/* Sisältö Etusivu-välilehdelle */}
+          <div className="search">
+            <input
+              placeholder="Search for movies"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <img
+              src={SearchIcon}
+              alt="search"
+              onClick={() => searchMovies(searchTerm)}
+            />
+          </div>
+
+          {movies.length > 0 ? (
+            <div className="container">
+              {movies.map((movie, index) => (
+                <MovieCard key={index} movie={movie} />
+              ))}
+            </div>
+          ) : (
+            <div className="empty">
+              <h2>No movies found</h2>
+            </div>
+          )}
         </div>
       )}
+
+      {/* Kirjautumisvälilehden sisältö */}
+      {activeTab === 'auth' && <Login />}
     </div>
   );
 };
